fba749f5adcc851824cc016aec29b49c3a9bccd2
BT_3W2_32 <cassandrafeinberg@gmail.com>
2024-12-06 13:11:37 -0500

    working on zoo

--- src/Zoo.java
+++ src/Zoo.java
@@ -3,33 +3,30 @@ import java.util.LinkedList;
 public class Zoo {
     static int employeeCount;
     static int animalCount;
+    static LinkedList<Animal> animals = new LinkedList<Animal>();
+    static LinkedList<Employee> employees = new LinkedList<Employee>();
+
+    public static void addAnimal(Animal animal){
+        animals.add(animal);
+        animalCount += 1;
+    }
+    public static void addEmployee(Employee employee){
+        employees.add(employee);
+        employeeCount += 1;
+    }
     public static void main(String[] args) {
-        LinkedList<Animal> animals = new LinkedList<Animal>();
-        LinkedList<Employee> employees = new LinkedList<Employee>();
-        Dog fluffy = new Dog("fluffy", true, 25.4, 2, "10/12", true);
-        animals.add(fluffy); animalCount += 1;
-        Sheep wooly = new Sheep("wooly", false, 78.9, 4, "7/5", "white");
-        animals.add(wooly); animalCount += 1;
-        Animal giraffy = new Animal();
-        animals.add(giraffy); animalCount += 1;
-        Animal wolfy = new Animal("wolf", "wolfy", true, 150.1, 7, "the dawn of time");
-        animals.add(wolfy); animalCount += 1;
+        addAnimal(new Dog("fluffy", true, 25.4, 2, "10/12", true));
+        addAnimal(new Sheep("wooly", false, 78.9, 4, "7/5", "white"));
+        addAnimal(new Animal());
+        addAnimal(new Animal("wolf", "wolfy", true, 150.1, 7, "the dawn of time"));
         System.out.println(animals);
-        Employee bobby = new Employee("bobby", 32, true, true, 7);
-        Employee mia = new Employee("mia", 19, false, false, 1);
+        addEmployee(new Employee("bobby", 32, true, true, 7));
+        addEmployee(new Employee("mia", 19, false, false, 1));
         Employee maya = new Employee();
         maya.setName("maya");
         maya.setAge(21);
         maya.setManager();
-        employees.add(bobby); employeeCount += 1;
-        employees.add(mia); employeeCount += 1;
-        employees.add(maya); employeeCount += 1;
+        addEmployee(maya);
         System.out.println(employees);
-        wooly.makeNoise();
-        fluffy.makeNoise();
-        giraffy.makeNoise();
-        wolfy.makeNoise();
-        System.out.println(wooly.getName() + " has " + wooly.getWoolColor() + " colored wool");
-        System.out.println(fluffy.getName() + " has spots? " + fluffy.hasSpots());
     }
 }
